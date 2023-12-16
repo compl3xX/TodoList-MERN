623929a913789dad6fbef13ae1e9e5b1d3c8a500
Rishu Tiwari <86428716+compl3xX@users.noreply.github.com>
2023-12-16 14:44:15 +0530

    deploy-config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://todo-app-backend-i3mo.onrender.com',
         changeOrigin: true,
       }
     }
