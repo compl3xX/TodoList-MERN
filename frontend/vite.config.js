ed4dbc6c197750fcb95e2895dbe730aa25ee0341
Rishu Tiwari <86428716+compl3xX@users.noreply.github.com>
2023-12-16 17:45:41 +0530

    proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://todo-app-backend-i3mo.onrender.com',
-        changeOrigin: true,
-      }
-    }
-  },
   plugins: [react()],
 })
